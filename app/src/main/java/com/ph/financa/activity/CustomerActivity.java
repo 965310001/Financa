260d6ead1d28f1c0951926902a5b8ccc0258005a
guofeng <965310001@qq.com>
2019-09-12 18:35:01 +0800

    调试我的客服

--- app/src/main/java/com/ph/financa/activity/CustomerActivity.java
+++ app/src/main/java/com/ph/financa/activity/CustomerActivity.java
@@ -1,7 +1,14 @@
 package com.ph.financa.activity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.widget.AppCompatEditText;
 
 import com.aries.ui.view.title.TitleBarView;
 import com.ph.financa.R;
@@ -42,5 +49,34 @@ public class CustomerActivity extends BaseTitleActivity {
         }
 
 
+        TextView mTvSend = findViewById(R.id.tv_send);
+        AppCompatEditText mEtInput = findViewById(R.id.et_input);
+        mEtInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (editable.length() > 0) {
+                    mTvSend.setBackgroundResource(R.drawable.shape_send_selector);
+                    mTvSend.setTextColor(Color.parseColor("#FFFFFF"));
+                } else {
+                    mTvSend.setBackgroundResource(R.drawable.shape_send_nomal);
+                    mTvSend.setTextColor(Color.parseColor("#BBBBBB"));
+                }
+            }
+        });
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.tv_send:
+                break;
+        }
     }
 }
